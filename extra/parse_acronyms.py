db47695bb7d866741493b4f6b6d9534eca169fd8
Eleni Mijalis <elenimijalis@gmail.com>
2017-01-18 13:24:47 -0600

    done

--- extra/parse_acronyms.py
+++ extra/parse_acronyms.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import argparse
+import re
 
 
 def parse_acronyms(data):
-    pass
+    for d in data:
+        splitline = re.findall(r'{[^{}]*}', d.strip('\\newacronym'))
+        print '\t'.join([splitline[0].strip('{').strip('}'), splitline[2].strip('{').strip('}')])
 
 
 if __name__ == '__main__':
